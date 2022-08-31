435a15422f508272e5c351fc471150996fa36530
VerTicaL@zlm <turnout@aliyun.com>
2022-08-31 13:36:40 +0800

    normalize

--- GE/fastinterp_test.py
+++ GE/fastinterp_test.py
@@ -1,15 +1,15 @@
+# coding: utf-8
+from genericpath import isfile
 import numpy as np
 import matplotlib.pyplot as plt
-import tkinter,time
+import tkinter,time,os
 from tkinter import *
 from tkinter.filedialog import askopenfilename
 import matplotlib.image as mpimage
 from PIL import Image
-# coding: utf-8
-import csv
+import csv,cv2
 from scipy import interpolate
 import matplotlib.cm as cm
-
 def fastinterp1(x, y, xi):
     ixi = np.digitize(xi, x)
     n = len(x)
@@ -18,6 +18,57 @@ def fastinterp1(x, y, xi):
     yi = (1-t) * y[ixi-1] + t * y[ixi]
     yi = yi.T
     return yi
+
+def open_tif():
+    """open a tif image save by GE CCD
+    return the image path
+    """
+    root = Tk()
+    root.withdraw()
+    root.update()
+    img_path = askopenfilename(title=u'Read CCD image')
+    #bg_path = askopenfilename(title=u'Read background image')
+    root.destroy()
+    return img_path if os.path.isfile(img_path) and img_path.endswith(".tif") else None
+
+def visualize_tif(img_src:str):
+    """view the input image
+
+    Args:
+        img_src (str): path to tif image
+    """
+    if os.path.isfile(img_src) and img_src.endswith(".tif"):
+        img = Image.open(img_src)
+        raw_matrix = np.array(img,dtype=np.float32)
+        fig1 = plt.figure(figsize =(16, 9))
+        fig1.canvas.manager.window.setWindowTitle("Visualize raw image")
+        plt.subplot(2,2,1),plt.imshow(raw_matrix,cmap=cm.rainbow,vmin=1300,vmax=1400)
+        plt.colorbar(location='right', fraction=0.1),plt.title("raw image")
+        #matrix1 = matrix.T
+        filter_N=3
+        #mean_matrix=cv2.medianBlur(raw_matrix, filter_N)
+        #mean_matrix = cv2.GaussianBlur(raw_matrix, (5, 5), 1)
+        #mean_matrix = cv2.blur(raw_matrix, (3, 3))
+        # 方框滤波（归一化）=均值滤波
+        mean_matrix = cv2.boxFilter(raw_matrix, -1, (3, 3), normalize=True)
+        plt.subplot(2,2,2),plt.imshow(mean_matrix,cmap=cm.rainbow,vmin=1300,vmax=1400)
+        plt.colorbar(location='right', fraction=0.1),plt.title("median blur image")
+        sum_rows_raw=np.sum(raw_matrix,axis=0)
+        sum_rows_mean=np.sum(mean_matrix,axis=0)
+        sum_cols_raw=np.sum(raw_matrix,axis=1)
+        sum_cols_mean=np.sum(mean_matrix,axis=1)
+        
+        plt.subplot(2,2,3),plt.title("sum cols")
+        plt.plot(sum_rows_raw)
+        plt.plot(sum_rows_mean)
+        plt.legend(['raw','median blur'])
+        
+        plt.subplot(2,2,4),plt.title("sum rows")
+        plt.plot(sum_cols_raw)
+        plt.plot(sum_cols_mean)
+        plt.legend(['raw','median blur'])
+        plt.show()
+
 
 if __name__ == "__main__":
     num=9
@@ -28,7 +79,9 @@ if __name__ == "__main__":
     print(len(y))
     y_interp=fastinterp1(x,y,xinterp)
     print(len(y_interp))
-    plt.plot(x,y,marker='o', markersize=4)
-    plt.plot(xinterp,y_interp,marker='x', markersize=1)
-    plt.legend(["initial","fast interpolate"])
-    plt.show()
+    img_src=open_tif()
+    visualize_tif(img_src)
+    # plt.plot(x,y,marker='o', markersize=4)
+    # plt.plot(xinterp,y_interp,marker='x', markersize=1)
+    # plt.legend(["initial","fast interpolate"])
+    # plt.show()
