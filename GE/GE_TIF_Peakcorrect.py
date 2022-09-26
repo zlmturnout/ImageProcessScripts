2236c4ca5121c9fa3bbb254b367f2cc026d5e686
VerTicaL@zlm <turnout@aliyun.com>
2022-09-26 20:27:02 +0800

    update save pics

--- GE/GE_TIF_Peakcorrect.py
+++ GE/GE_TIF_Peakcorrect.py
@@ -56,7 +56,7 @@ def gaussian(x, amp, cen, wid):
     return (amp / (sqrt(2*pi) * wid)) * exp(-(x-cen)**2 / (2*wid**2))
 
 
-def Gaussian_FWHM(x,y,center=1200):
+def Gaussian_FWHM(x,y,center=1200,index=1,info='Gaussian-FWHM_fit'):
     """find FWHM from the imported pd_data [x,y]
 
     Args:
@@ -66,27 +66,37 @@ def Gaussian_FWHM(x,y,center=1200):
     # x = pd_data.values[:, 0]
     # y = pd_data.values[:, 1]
     gmodel = Model(gaussian)
-    result = gmodel.fit(y, x=x, amp=150706, cen=center, wid=2.6)
+    result = gmodel.fit(y, x=x, amp=49146, cen=center, wid=2.6)
     print(result.values)
     print(result.fit_report())
     wid_fit=result.params['wid'].value
-    wid_err=result.params['wid'].stderr 
+    wid_err=result.params['wid'].stderr
+    cen_fit=result.params['cen'].value 
+    cen_err=result.params['wid'].stderr
     print(f'wid_err:{wid_err}')
-    FWHM=wid_fit*2*np.sqrt(np.log(4))
     if wid_err!= None:
+        FWHM=wid_fit*2*np.sqrt(np.log(4))
         FWHM_err=wid_err*2*np.sqrt(np.log(4))
+        cen_text=f'x0={cen_fit:4f} +/-{cen_err:.4f}\n'
+        wid_text=f'w={wid_fit:4f} +/-{wid_err:.4f}\n'
         FWHW_text=f'FWHM={FWHM:.4f} +/-{FWHM_err:.4f}'
     else:
         FWHM_err='estimate failed'
+        cen_text=f'x0={cen_fit:4f} estimate failed\n'
+        wid_text=f'w={wid_fit:4f} estimate failed\n'
         FWHW_text=f'FWHM={FWHM:.4f} +/-{FWHM_err}'
     print(f'get FWHM={FWHM:.4f} with error +/-{FWHM_err}')
     fig=plt.figure(figsize =(16, 9))
+    fig.canvas.manager.window.setWindowTitle(f"Fit-FWHM-{index}")
     ax=plt.subplot()
     plt.plot(x, y, 'o')
     plt.plot(x, result.init_fit, '--', label='initial fit')
     plt.plot(x, result.best_fit, '-', label='best fit')
-    plt.text(0.5, 0.5, s=FWHW_text,color = "m", transform=ax.transAxes,fontsize=15)
+    plt.text(0.5, 0.5, s=cen_text+wid_text+FWHW_text,color = "m", transform=ax.transAxes,fontsize=15)
+    plt.text(0.5, 1.0, s=f'Gauss FIt-{index}',color = "m", transform=ax.transAxes,fontsize=15)
     plt.legend()
+    save_fig=os.path.join(save_folder,f'FWHM_fit-{filename}-{index}.jpg')
+    plt.savefig(save_fig)
     return FWHM,FWHM_err
 
 
@@ -116,10 +126,11 @@ sum_cols_raw=np.sum(matrix,axis=1)
 col_index=[j for j in range(len(sum_cols_raw)) ]
 plt.subplot(2,2,3),plt.plot(row_index,sum_rows_raw),plt.title("sum cols")
 plt.subplot(2,2,2),plt.plot(col_index,sum_cols_raw),plt.title("sum rows")
-
+save_fig=os.path.join(save_folder,f'VisualizeRawImage-{filename}.jpg')
+plt.savefig(save_fig)
 
 # selected point near the mid of the line
-p_col=1126
+p_col=1084
 p_row=1042
 half_n=200   # total 2*half_n rows for correction
 '''
@@ -168,6 +179,8 @@ plt.subplot(3,3,6),plt.hist(out.flatten(),bins=200),plt.title("intensity histogr
 sum_cols_cut=np.sum(out.T,axis=1)
 col_index=[j for j in range(len(sum_cols_cut)) ]
 plt.subplot(3,3,9),plt.plot(col_index,sum_cols_cut),plt.title("sum cols")
+save_fig=os.path.join(save_folder,f'imageDataPreprocess-{filename}.jpg')
+plt.savefig(save_fig)
 #plt.show()
 
 # line correction
@@ -175,8 +188,8 @@ plt.subplot(3,3,9),plt.plot(col_index,sum_cols_cut),plt.title("sum cols")
 def shift_pixel(index:int,j:int=1):
     #return round(0.005 + index*0.01+index**2*(1+j*0.1)*1e-7)
     #return round((0.001+0.005*j)*index+index**2*(1e-7))
-    return round((0.015+0.0005*j)*index+index**2*(1e-7)) # best fit results
-
+    #return round((0.015+0.0005*j)*index+index**2*(1e-7)) # best fit results
+    return round((-0.0007+0.00005*j)*index+index**2*(1e-7))
 header_list=['pixels']
 
 cor_matrix = out
@@ -193,7 +206,7 @@ low_lim = round(index*20 -1200)
 high_lim = round(index*20 + 1200)
 xinitial = np.arange(n)
 corrected_list.append(xinitial[round(low_lim/20):round(high_lim/20)]-half_n+p_col)
-for j in range(0,5,1):
+for j in range(-5,5,1):
     low_lim = round(index*20 -1200)
     high_lim = round(index*20 + 1200)
     new_X = np.arange(low_lim, high_lim)
@@ -215,41 +228,47 @@ for j in range(0,5,1):
     
     corrected_list.append(y_[round(low_lim/20):round(high_lim/20)])
     header_list.append(f"intensity{j+1}")
-    plt.subplot(2,1,1),plt.plot(xinitial[round(low_lim/20):round(high_lim/20)]-half_n+p_col,
-        y_[round(low_lim/20):round(high_lim/20)])
-    plt.title("correction via shift right+")
-    plt.pause(0.5)
+    if j <0:
+        plt.subplot(2,1,1),plt.plot(corrected_list[0],result)
+        plt.title("correction via shift left-")
+        plt.pause(0.1)
+    else:
+        plt.subplot(2,1,2),plt.plot(corrected_list[0],result)
+        plt.title("correction via shift right+")
+        plt.pause(0.1)
     plt.legend([i for i in range(10)])
+save_fig=os.path.join(save_folder,f'Visualize peak correction-{filename}.jpg')
+plt.savefig(save_fig)
 
 # right move rows, dd = shift_pixel(ii,-j)
-for j in range(0,5,1):
-    low_lim = round(index*20 -1200)
-    high_lim = round(index*20 + 1200)
-    new_X = np.arange(low_lim, high_lim)
-    result = np.zeros(len(new_X))
-    xinitial = np.arange(n)
-    xinterp = np.arange(0, n, 0.05)
-    xx = np.linspace(0, len(xinterp), n)
-    for ii in range(m):
-        temp =cor_matrix[ii, :]
-        ntemp = fastinterp1(xinitial, temp, xinterp)
-        dd= shift_pixel(ii,-j)
-        #print(f'shift pixels: {dd} with j={j} and index={ii}')
-        new_img[ii,:] = fastinterp1(new_X,ntemp[low_lim-dd:high_lim-dd],xx)
-        result = result + ntemp[low_lim-dd:high_lim-dd]
-    print(result)
-    y_ = fastinterp1(new_X, result, xx)
-    # append data
-    corrected_list.append(y_[round(low_lim/20):round(high_lim/20)])
-    header_list.append(f"intensity_-{j+1}")
-    plt.subplot(2,1,2),plt.plot(xinitial[round(low_lim/20):round(high_lim/20)]-half_n+p_col,
-        y_[round(low_lim/20):round(high_lim/20)])
-    plt.title("correction via shift left-")
-    plt.pause(0.1)
-plt.legend([i for i in range(10)])
+# for j in range(0,5,1):
+#     low_lim = round(index*20 -1200)
+#     high_lim = round(index*20 + 1200)
+#     new_X = np.arange(low_lim, high_lim)
+#     result = np.zeros(len(new_X))
+#     xinitial = np.arange(n)
+#     xinterp = np.arange(0, n, 0.05)
+#     xx = np.linspace(0, len(xinterp), n)
+#     for ii in range(m):
+#         temp =cor_matrix[ii, :]
+#         ntemp = fastinterp1(xinitial, temp, xinterp)
+#         dd= shift_pixel(ii,-j)
+#         #print(f'shift pixels: {dd} with j={j} and index={ii}')
+#         new_img[ii,:] = fastinterp1(new_X,ntemp[low_lim-dd:high_lim-dd],xx)
+#         result = result + ntemp[low_lim-dd:high_lim-dd]
+#     print(result)
+#     y_ = fastinterp1(new_X, result, xx)
+#     # append data
+#     corrected_list.append(y_[round(low_lim/20):round(high_lim/20)])
+#     header_list.append(f"intensity_-{j+1}")
+#     plt.subplot(2,1,2),plt.plot(xinitial[round(low_lim/20):round(high_lim/20)]-half_n+p_col,
+#         y_[round(low_lim/20):round(high_lim/20)])
+#     plt.title("correction via shift left-")
+#     plt.pause(0.1)
+#     plt.legend([i for i in range(5)])
 
 # save corrected_list data
-corr_datafile=os.path.join(save_folder,f'Peakcorrected_half_n1126_square-{half_n}-{filename}.xlsx')
+corr_datafile=os.path.join(save_folder,f'Peakcorrected_half_n-{half_n}-{filename}.xlsx')
 
 corrected_data=np.array(corrected_list,dtype=np.float32).T
 # save to excel
@@ -261,7 +280,7 @@ x_correct=pd_data.values[:, 0]
 FWHM_results={}
 for i in range(10):
     y_correct=pd_data.values[:, i+1]
-    FWHM,FWHM_err=Gaussian_FWHM(x_correct,y_correct,center=p_col)
+    FWHM,FWHM_err=Gaussian_FWHM(x_correct,y_correct,center=p_col,index=i,info=filename)
     print(f'fit-{i}:{FWHM:.4f},{FWHM_err}\n')
     FWHM_results[f'Fit-{i}']=(FWHM,FWHM_err)
 for key,value in FWHM_results.items():
@@ -270,5 +289,5 @@ excel_writer = pd.ExcelWriter(corr_datafile)
 pd_data.to_excel(excel_writer)
 excel_writer.save()
 print(f'save to excel xlsx file successfully\nfile path: {corr_datafile}')
-plt.show()
+#plt.show()
 
